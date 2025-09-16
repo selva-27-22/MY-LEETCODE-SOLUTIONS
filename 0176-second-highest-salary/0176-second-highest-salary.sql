a1d0ddfa949670632e9afe83b8d67e0fbea91b4c
selva-27-22 <kumarmuthu7014@gmail.com>
2025-09-16 09:38:24 +0530

    Time: 284 ms (38.68%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,3 @@
--- SELECT DISTINCT salary  as SecondHighestSalary
--- FROM Employee
--- ORDER BY  salary DESC
--- LIMIT 1 OFFSET 1
--- limit set limit,offset for skip top 1 ,distinct to remove duplicates,
--- automatically fill null if notexists
 SELECT IFNULL( 
 (SELECT DISTINCT salary  
 FROM Employee
