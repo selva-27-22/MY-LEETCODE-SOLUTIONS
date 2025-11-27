53a0da94a4c1900b91d24563ce6062c805b3500c
selva-27-22 <kumarmuthu7014@gmail.com>
2025-11-27 14:31:38 +0530

    Time: 14 ms (97.87%), Space: 46.5 MB (31.91%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.java
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] executeInstructions(int n, int[] pos, String s) {
         int len=s.length();
-        int result[]=new int[len];
+        int result[]=new int[len];char chars[]=s.toCharArray();
         int row=pos[0];
         int col=pos[1];
         int count=0;
@@ -10,7 +10,7 @@ class Solution {
             col=pos[1];
             count=0;;
             for(int j=i;j<len;j++){
-                char ch=s.charAt(j);
+                char ch=chars[j];
                 if(ch=='U')row--;
                 else if(ch=='D')row++;
                 else if(ch=='L')col--;
